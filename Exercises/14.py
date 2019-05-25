fb3bf04daf44b93a01f0e4fa684770b1bb611ef9
Wuyulp <1541435637@qq.com>
2019-05-25 16:23:26 +0800

    Focus

---
+++ Exercises/14.py
@@ -0,0 +1,47 @@
+from math import *
+from time import clock
+"""
+clock()
+i=1
+n=1
+t=1
+k=-1
+sum=0
+while i<100000000:
+	sum+=t	
+	t=(1/(n+2))*k
+	k=-k
+	n+=2
+	i+=1
+print(sum*4)
+print(pi)
+print(clock())
+
+print()
+clock()
+i=1
+n=1
+t=1
+sum=0.0
+while i<10000000:
+	sum+=t
+	t=t*(i/(n+2))
+	n+=2
+	i+=1
+print(sum*2)
+print(clock())
+"""
+N=1010
+WIDTH=N-10
+a,b=1,3
+c,d,i=0,0,0
+run,cnt,cntMAX=1,1,1000000
+x,z=[],[]
+for i in range(N):
+	x.append(0)
+	z.append(0)
+x[1]=2
+z[1]=2
+while (run and cnt<cntMAX):
+	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+
